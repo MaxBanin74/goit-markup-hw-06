129880a2ee649b31a329298d993944c39f66f330
Max Banin <banin@ukr.net>
2026-02-15 01:27:33 +0200

    6

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,11 @@
 (() => {
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector("[mobile-menu-open]"),
+    openModalBtn: document.querySelector("[data-mobile-menu-open]"),
     // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector("[mobile-menu-close]"),
+    closeModalBtn: document.querySelector("[data-mobile-menu-close]"),
     // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector("[mobile-menu]"),
+    modal: document.querySelector("[data-mobile-menu]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
